7c1d05cda68587551fbd6b34ae0579ac39c8649e
Jamie Barton <hi_jamie@me.com>
2017-11-26 18:12:42 +0000

    Updated to apollo client 2.0

--- src/index.js
+++ src/index.js
@@ -1,41 +1,26 @@
-import React from 'react'
-import { render } from 'react-dom'
-import {
-  ApolloProvider,
-  ApolloClient,
-  createNetworkInterface
-} from 'react-apollo'
+import React from 'react';
+import { render } from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
 
-import App from './App'
-
-const networkInterface = createNetworkInterface({
-  uri: process.env.REACT_APP_GRAPHQL_URI
-})
-
-networkInterface.use([{
-  applyMiddleware(req, next) {
-    let token = localStorage.getItem('token')
-
-    req.options.headers = {
-      authorization: token ? `Bearer ${token}` : null,
-      ...req.options.headers
-    }
-
-    next()
-  }
-}])
+import App from './App';
 
 const client = new ApolloClient({
-  networkInterface,
-  dataIdFromObject: o => o.id,
-  connectToDevTools: process.env.NODE_ENV === 'development'
-})
+  link: new HttpLink({
+    uri: process.env.REACT_APP_GRAPHQL_URI
+  }),
+  cache: new InMemoryCache(),
+  connectToDevTools: process.env.NODE_ENV === 'development',
+  dataIdFromObject: o => o.id
+});
 
-const target = document.querySelector('#root')
+const target = document.querySelector('#root');
 
 render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
   target
-)
+);
